c365c64f5edd68ef2d7e1d0ae1676b3bd60d4554
joycendc <capilijoycen0@gmail.com>
2021-02-04 18:22:59 +0800

    adding files

---
+++ js/app.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+  const preload = document.querySelector('.preload');
+  preload.classList.add('preload-finish');
+  
+});
+
+const texts = ['Studying_BSIT','a_Java_Programmer','a_Speed_Cuber'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+  if(count === texts.length){
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+}());
